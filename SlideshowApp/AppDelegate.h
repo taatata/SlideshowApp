ca10aba393d731f31f78290525a481393865dd42
taatata <taatatatati@gmail.com>
2015-11-27 20:52:49 +0900

    Initial Commit

---
+++ SlideshowApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideshowApp
+//
+//  Created by Daewoo Song on 2015/11/27.
+//  Copyright © 2015年 taro.kirameki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
